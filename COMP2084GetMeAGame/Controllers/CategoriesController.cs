2a6b77d4387a0c06a76659597eeb8417b3172832
HrudyaR <hrudya.jayaprakash@gmail.com>
2020-10-01 15:37:47 -0400

    AddCategory form

--- COMP2084GetMeAGame/Controllers/CategoriesController.cs
+++ COMP2084GetMeAGame/Controllers/CategoriesController.cs
@@ -11,11 +11,17 @@ namespace COMP2084GetMeAGame.Controllers
     {
         public IActionResult Index()
         {
+            //User the category model to create 10 fake categories and send to the view for display
+            //Create an empty list of categories
             var categories = new List<Category>();
+
+            //Use a loop to create 10 fake categories
             for (var i = 1; i <= 10; i++)
             {
                 categories.Add(new Category { Id = 1, Name = "Category" + i.ToString() });
             }
+
+            //Pass the final list to the view for display
             return View(categories);
         }
 
@@ -23,6 +29,12 @@ namespace COMP2084GetMeAGame.Controllers
         {
             //take the category name passed in with the link and store it in the viewbag for display
             ViewBag.categoryName = categoryName;
+            return View();
+        }
+
+        public IActionResult AddCategory()
+        {
+            //Display an empty form where user could add new category
             return View();
         }
     }
