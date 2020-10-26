763516afc959e60a1d3aa30498335002cbbf791f
Jyaprakash <jayaprakash.av@block64.com>
2020-10-26 12:29:44 -0400

    Authorization added

---
+++ COMP2084GetMeAGame/Controllers/ShopController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using COMP2084GetMeAGame.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2084GetMeAGame.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext _contaxt;
+
+        public ShopController(ApplicationDbContext context)
+        {
+            _contaxt = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _contaxt.Categories.OrderBy(c => c.Name).ToList();
+            return View(categories);
+        }
+    }
+}
